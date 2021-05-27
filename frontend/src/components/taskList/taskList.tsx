50c0a1900480216d63a682a3072bddc52e5dbdd6
Fresh222 <ksz2520@gmail.com>
2021-05-28 00:31:35 +0200

    add collapse feature

--- frontend/src/components/taskList/taskList.tsx
+++ frontend/src/components/taskList/taskList.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
+// import TableCell from "@material-ui/core/TableCell";
+import MuiTableCell from '@material-ui/core/TableCell';
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import "./taskList.css";
-import { IconButton } from "@material-ui/core";
+import {Box, Collapse, IconButton, Typography, withStyles} from "@material-ui/core";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import TableRowComponent from "./taskListRow";
+
+const TableCell = withStyles(theme => ({
+  root: {
+    height: 10,
+    padding:3,
+    borderColor: '#C8C8C8',
+    borderTop: "1px",
+  }
+}))(MuiTableCell);
 
 function createData(
   color: string,
@@ -36,12 +46,12 @@ const rows = [
 export default function BasicTable() {
   const [state, setState] = React.useState({
     inProgressColor: {
-      backgroundColor: "yellow",
+      backgroundColor: "#FFEF62",
       fontWeight: "bold" as "bold",
     },
-    doneColor: { backgroundColor: "green", fontWeight: "bold" as "bold" },
-    toDoColor: { backgroundColor: "grey", fontWeight: "bold" as "bold" },
-    pausedColor: { backgroundColor: "red", fontWeight: "bold" as "bold" },
+    doneColor: { backgroundColor: "#33EB91", fontWeight: "bold" as "bold" },
+    toDoColor: { backgroundColor: "#CFCFCF", fontWeight: "bold" as "bold" },
+    pausedColor: { backgroundColor: "#F1503A", fontWeight: "bold" as "bold" },
   });
 
   const [open, setOpen] = React.useState(false);
@@ -50,7 +60,7 @@ export default function BasicTable() {
     <TableContainer className="table-main" component={Paper}>
       <Table className="table" aria-label="simple table">
         <TableHead>
-          <TableRow>
+          <TableRow style={{ height: 'auto !important', borderTop: '10px', borderColor: 'yellow' }}>
             {/* cell z kolorkiem */}
             <TableCell className="color-rec-head"></TableCell>
             <TableCell></TableCell>
@@ -78,13 +88,13 @@ export default function BasicTable() {
             }
 
             return (
-              <TableRowComponent key={row.name} {...row} style={style} />
+                <TableRowComponent key={row.name} {...row} style={style} ></TableRowComponent>
             );
           })}
 
           {/*  adding row siup */}
           {/*<TableRowComponent name={"+ Dodaj"} person={""} status={""} style={{}} />*/}
-          <TableRow>
+          <TableRow style={{ height: '10px !important' }} >
             <TableCell id={"color"} className="color-rec">
               <IconButton
                 aria-label="expand row"
@@ -94,10 +104,51 @@ export default function BasicTable() {
                 {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
               </IconButton>
             </TableCell>
-            <TableCell className="add-row-text">+ Dodaj</TableCell>
+            <TableCell className="add-row-text"><div style={{ color: "#979797" }}>+ Dodaj</div></TableCell>
             <TableCell className="add-row-text"></TableCell>
             <TableCell className="add-row-text"></TableCell>
           </TableRow>
+
+
+
+
+          <TableRow>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <Box margin={1}>
+                  <Typography variant="h6" gutterBottom component="div">
+                    Historyyy
+                  </Typography>
+                  <Table size="small" aria-label="purchases">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Customer</TableCell>
+                        <TableCell align="right">Amount</TableCell>
+                        <TableCell align="right">Total price ($)</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    {/*<TableBody>*/}
+                    {/*  {row.history.map((historyRow) => (*/}
+                    {/*      <TableRow key={historyRow.date}>*/}
+                    {/*        <TableCell component="th" scope="row">*/}
+                    {/*          {historyRow.date}*/}
+                    {/*        </TableCell>*/}
+                    {/*        <TableCell>{historyRow.customerId}</TableCell>*/}
+                    {/*        <TableCell align="right">{historyRow.amount}</TableCell>*/}
+                    {/*        <TableCell align="right">*/}
+                    {/*          {Math.round(historyRow.amount * row.price * 100) / 100}*/}
+                    {/*        </TableCell>*/}
+                    {/*      </TableRow>*/}
+                    {/*  ))}*/}
+                    {/*</TableBody>*/}
+                  </Table>
+                </Box>
+              </Collapse>
+            </TableCell>
+          </TableRow>
+
+
         </TableBody>
       </Table>
     </TableContainer>
--- frontend/src/components/taskList/taskListRow.tsx
+++ frontend/src/components/taskList/taskListRow.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import {IconButton} from "@material-ui/core";
+import {
+    Box,
+    Collapse,
+    IconButton, Typography,
+    withStyles
+} from "@material-ui/core";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import "./taskListRow.css"
 import "./taskList.css"
+import MuiTableCell from "@material-ui/core/TableCell";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+
+const TableCell = withStyles(theme => ({
+    root: {
+        height: 10,
+        padding:3,
+        // margin: "10px 0px 10px 0px",
+        // marginTop: 10,
+        // paddingTop: 10,
+        // borderSpacing: '0 5px',
+        // borderCollapse: 'separate',
+        // border-spacing: 0px 50px,
+    }
+}))(MuiTableCell);
 
 export default function TableRowComponent(props: { name: {} | null | undefined;
     person: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined;
@@ -13,10 +34,10 @@ export default function TableRowComponent(props: { name: {} | null | undefined;
     style: React.CSSProperties | undefined;}
     ) {
     const [open, setOpen] = React.useState(false);
-
     // console.log(styleProp)
 
     return (
+        <>
       <TableRow className="row" >
           <TableCell id={"color"} className="color-rec">
               <IconButton
@@ -43,5 +64,42 @@ export default function TableRowComponent(props: { name: {} | null | undefined;
               {props.status}
           </TableCell>
       </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Historyyy
+                            </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Date</TableCell>
+                                        <TableCell>Customer</TableCell>
+                                        <TableCell align="right">Amount</TableCell>
+                                        <TableCell align="right">Total price ($)</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                {/*<TableBody>*/}
+                                {/*  {row.history.map((historyRow) => (*/}
+                                {/*      <TableRow key={historyRow.date}>*/}
+                                {/*        <TableCell component="th" scope="row">*/}
+                                {/*          {historyRow.date}*/}
+                                {/*        </TableCell>*/}
+                                {/*        <TableCell>{historyRow.customerId}</TableCell>*/}
+                                {/*        <TableCell align="right">{historyRow.amount}</TableCell>*/}
+                                {/*        <TableCell align="right">*/}
+                                {/*          {Math.round(historyRow.amount * row.price * 100) / 100}*/}
+                                {/*        </TableCell>*/}
+                                {/*      </TableRow>*/}
+                                {/*  ))}*/}
+                                {/*</TableBody>*/}
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+            </>
+
   );
 }
