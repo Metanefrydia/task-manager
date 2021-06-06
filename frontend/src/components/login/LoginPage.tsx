1c876829f865e4687a8a1858cd21d9fe87e38236
Wiktoria <wiktoria.kalka@netsoftware.pl>
2021-06-06 18:35:31 +0200

    /register-page fixed comments from Marta

--- frontend/src/components/login/LoginPage.tsx
+++ frontend/src/components/login/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Login.css";
 import LoginCard from "./LoginCard";
 
 interface State {}
@@ -8,7 +7,7 @@ interface Props {}
 export class LoginPage extends React.Component<Props, State> {
   render() {
     return (
-      <div className="back-image">
+      <div>
         <LoginCard />
       </div>
     );
