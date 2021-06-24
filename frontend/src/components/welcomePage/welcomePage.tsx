9ebf1294a6e5ebbe8769be04c5ff4130ecd139bb
Fresh222 <ksz2520@gmail.com>
2021-06-24 19:27:12 +0200

    delete welcomePage component

--- frontend/src/components/welcomePage/welcomePage.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-export default function WelcomePage() {
-  const [state, setState] = useState();
-
-  return <div>Witaj, zaloguj sie lub utwórz konto</div>;
-}
