a6ba9fda249a3b410e08026d6e84c721c9d3c158
Wiktoria <wiktoria.kalka@netsoftware.pl>
2021-06-28 17:53:23 +0200

    /No_Group_Info added styles to info on home page

--- frontend/src/components/home/NoGroupCard.tsx
+++ frontend/src/components/home/NoGroupCard.tsx
@@ -1,15 +1,15 @@
 import {Link} from "@material-ui/core";
 import AuthenticationService from "../../services/AuthenticationService";
-
+import "./NoGroupCardStyle.css"
 
 export default function NoGroupCard() {
     const userId = AuthenticationService.getUserDetails()?._id;
 
 
     return (
-    <div>
-      <p>Brak Grup. <a href={`/groups/${userId}`}>Utwórz grupę</a> By dodać zadania
-      </p>
+    <div className="to-group-link-container">
+      <p className="to-group-link-des">Brak Grup.</p>
+        <p className="to-group-link-des"><a href={`/groups/${userId}`} className="to-group-link">Utwórz grupę</a> by dodać zadania</p>
 
 
     </div>
